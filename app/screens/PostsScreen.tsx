b89bbaeaec3fa21c3b7ba4adb8f191744d755eac
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-09-20 12:39:54 +0530

    fetch from server

--- app/screens/PostsScreen.tsx
+++ app/screens/PostsScreen.tsx
@@ -1,19 +1,56 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import type { RootStackParamList } from "../App";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const PostsScreen: React.FC<NativeStackScreenProps<RootStackParamList>> = ({
   navigation,
 }) => {
   return (
     <View style={styles.container}>
-      <Text>Welcome to posts</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate("Home")}
-      ></Button>
-      <StatusBar style="auto" />
+      <QueryClientProvider client={queryClient}>
+        <Text>Welcome to posts</Text>
+        <Posts />
+        <Button
+          title="Go to Home"
+          onPress={() => navigation.navigate("Home")}
+        ></Button>
+        <StatusBar style="auto" />
+      </QueryClientProvider>
+    </View>
+  );
+};
+
+const Posts = () => {
+  // Access the client
+  const queryClient = useQueryClient();
+  // Queries
+  const query = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const posts: { message: string }[] = await (
+        await fetch("http://localhost:3000/api/post")
+      ).json();
+      return posts;
+    },
+  });
+
+  return (
+    <View>
+      {!query.isSuccess ? (
+        <Text>Failed to Fetch</Text>
+      ) : (
+        <FlatList
+          data={query.data}
+          renderItem={({ item }) => (
+            <Text style={styles.item}>{item.message}</Text>
+          )}
+        />
+      )}
     </View>
   );
 };
@@ -24,6 +61,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
 
