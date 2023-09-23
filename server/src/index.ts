30599877d6a3a2b362240287e305a66b6b5a3776
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-09-23 13:21:04 +0530

    take env port

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,6 +15,6 @@ app.get("/ping", (req, res) => {
   return res.json({ message: "pong" });
 });
 
-app.listen(3000, () =>
+app.listen(process.env.PORT || 3000, () =>
   console.log("REST API server ready at: http://localhost:3000 ⚡⚡")
 );
