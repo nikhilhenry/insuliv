7b1339ddf6dba52d7326784e70521981da5fa654
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-09-23 12:42:24 +0530

    store image in memory

--- server/src/routes/image.ts
+++ server/src/routes/image.ts
@@ -3,24 +3,35 @@ import { Deta } from "deta";
 import { nanoid } from "nanoid";
 import multer from "multer";
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ storage: multer.memoryStorage() });
 const projectKey = process.env.DETA_KEY;
 const deta = Deta(projectKey);
 const photos = deta.Drive("food");
 
 export const imageRouter = Router();
 
+imageRouter.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const img = await photos.get(id);
+  if (!img) {
+    return res.status(404).json({ message: "Image not found" });
+  }
+  res.contentType("jpeg");
+  const imgData = await img.arrayBuffer();
+  return res.end(Buffer.from(imgData), "binary");
+});
+
 imageRouter.post("/upload", upload.single("file"), async (req, res) => {
   const file = req.file;
-  console.log(file);
-
-  const fileName = nanoid(4) + file?.filename.split(".").pop();
-
+  if (!file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+  const fileName = nanoid(4) + ".jpg";
   // load the image from the file system in the uploads folder
   const id = await photos.put(fileName, {
-    data: "uploads/" + file?.filename,
+    data: file.buffer,
   });
   return res
     .status(200)
-    .send({ img_url: "http://localhost:3000/api/image" + id });
+    .send({ img_url: "http://localhost:3000/api/image/" + id });
 });
