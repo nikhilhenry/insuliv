eaf79622767617b018cbe905692fd13ef3a32fd7
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-09-24 15:19:49 +0530

    create a file based state machine

--- server/src/lib/testFit.ts
+++ server/src/lib/testFit.ts
@@ -1,10 +1,17 @@
 import "dotenv/config";
 import { getActivities, getBPM, getSteps } from "./googleFit";
 import { getAuthToken } from "./getAccessToken";
+import { getLastSync, saveLastSync } from "./state";
 
-const activities = await getActivities();
-console.log(activities?.length);
-console.log(activities);
+// const activities = await getActivities();
+// console.log(activities?.length);
+// console.log(activities);
+
+// console.log(await getSteps());
 // console.log(activities[0]);
 // const date = new Date(Number(activities[0].startTime));
 // console.log(date.toLocaleDateString());
+
+console.log(getLastSync());
+saveLastSync();
+console.log(getLastSync());
