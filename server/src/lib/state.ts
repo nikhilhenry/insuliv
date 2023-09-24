56c65bfd0563b98d27b7535d9be86806ed6da9e4
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-09-24 22:24:06 +0530

    fix trailing slash

--- server/src/lib/state.ts
+++ server/src/lib/state.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const stateDir = path.join(__dirname, "../../state/");
+const stateDir = path.join(__dirname, "../../state");
 
 const writeToFile = (file: string, data: string) => {
   try {
