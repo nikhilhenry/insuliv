35f1f12b80cf9ed3e5d8b760b40f4cac4329241b
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-09-27 13:55:58 +0530

    create image handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 
 import "dotenv/config";
+import { imageRouter } from "./routes/imageRouter";
+import { postRouter } from "./routes/post";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/image", imageRouter);
+app.use("/api/post", postRouter);
 
 app.get("/ping", (req, res) => {
   return res.json({ message: "pong" });
