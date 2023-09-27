72b454e1f356a35177a505af2026d40722eb095f
Anshuman <anshuman.sharma@plaksha.edu.in>
2023-09-27 19:34:37 +0530

    added metarouter for daily driver data of a week

--- server/src/routes/metaRouter.ts
+++ server/src/routes/metaRouter.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { prisma } from "../lib/prisma";
+import { getCalories } from "../lib/googleFit";
+import { start } from "repl";
 
 export const metaRouter = Router();
 
@@ -18,9 +20,59 @@ metaRouter.get("/summary", async (req, res) => {
   });
 });
 
-metaRouter.get("/", async (req, res) => {
+metaRouter.get("/weekly", async (req, res) => {
   let startTime: Date = new Date();
   const date = new Date();
-  date.setDate(date.getDate() - 7);
+  date.setHours(0,0,0,0)
   startTime = date;
+
+  const average_Calories = [];
+  const return_data = [];
+
+  for(let i=0;i<7;i++){ 
+    date.setDate(date.getDate() - i);
+    startTime = date;
+    const carbs=await prisma.foodActivity.findMany({
+      where: {createdAt: { gte: startTime } }
+    })
+
+    var sum_calories=0;
+    carbs.forEach((item)=>sum_calories+=Number(item.Carbs));
+    average_Calories.push(sum_calories/carbs.length);
+  }
+  return_data.push({category: "Carbs",
+  data: average_Calories});
+
+
+  var calories = await getCalories();
+  var daily_cal=[]
+
+  calories?.forEach((item)=>
+  daily_cal.push(Number(item.calories))
+  )
+
+  return_data.push({category: "Calories",
+  data: daily_cal});
+
+  date.setHours(0,0,0,0)
+  startTime = date;
+
+  const bpm_per_day=[];
+
+  for(let i=0;i<7;i++)
+  {
+    date.setDate(date.getDate() - i);
+    startTime = date;
+    const bpm=await prisma.bPMPerDay.findMany({
+      where: {recordedAt: { gte: startTime } }
+    });
+
+    bpm_per_day.push(bpm[0].value);
+  }
+
+  return_data.push({category: "Heart Rate",
+data: bpm_per_day})
+
+
+  return res.send(return_data);
 })
