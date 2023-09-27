e05b2d7230ff45be66ffb8e1f9f959626fe9971a
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-09-27 19:43:22 +0530

    fetch from api

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import { imageRouter } from "./routes/imageRouter";
 import { postRouter } from "./routes/post";
 import { activityRouter } from "./routes/acitivityRouter";
+import { metaRouter } from "./routes/metaRouter";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use("/api/image", imageRouter);
 app.use("/api/post", postRouter);
 app.use("/api/activity", activityRouter);
+app.use("/api/meta", metaRouter);
 
 app.get("/ping", (req, res) => {
   return res.json({ message: "pong" });
