104919bc09a9030d0f6370d4c5a57c5b4780258b
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-09-27 14:36:49 +0530

    create a food router

---
+++ server/src/routes/foodRouter.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { prisma } from "../lib/prisma";
+import { allowedNodeEnvironmentFlags } from "process";
+
+export const foodRouter = Router();
+
+foodRouter.get("/", async (req, res) => {
+  const foodlog = await prisma.foodActivity.findMany();
+  return res.json(foodlog);
+});
+
+foodRouter.post("/", async (req, res, next) => {
+  try {
+    const food = await prisma.foodActivity.create({
+      data: req.body,
+    });
+    res.json(food);
+  } catch (error) {
+    next(error);
+  }
+});
+
+foodRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedFood = await prisma.foodActivity.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    res.json(deletedFood);
+  } catch (error) {
+    next(error);
+  }
+});
